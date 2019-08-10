dfd3a480e2c91ea3a65bf093ffd5fe696a60a781
bu.liwen <bu.liwen@chinaott.net>
2019-08-10 16:36:11 +0800

    单端设置--只consumer端设置超时熔断机制，provider端调用会报500错误

--- provider/src/main/java/com/dubbo/provider/service/CostServiceImpl.java
+++ provider/src/main/java/com/dubbo/provider/service/CostServiceImpl.java
@@ -2,6 +2,8 @@ package com.dubbo.provider.service;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.dubbo.api.service.CostService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @Service
 public class CostServiceImpl implements CostService {
@@ -16,8 +18,19 @@ public class CostServiceImpl implements CostService {
      * @param cost
      * @return
      */
+    @HystrixCommand(commandProperties = {
+            //请求量
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),
+            //请求超时时间
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
+    })
     @Override
     public Integer add(int cost) {
+        try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return totalCost + cost;
     }
 }
