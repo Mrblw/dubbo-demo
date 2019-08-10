0e02c943d119ababf64512aa175477d26341e2c3
bu.liwen <bu.liwen@chinaott.net>
2019-08-10 17:29:18 +0800

    provider端轮询机制设置

--- provider/src/main/java/com/dubbo/provider/service/CostServiceImpl.java
+++ provider/src/main/java/com/dubbo/provider/service/CostServiceImpl.java
@@ -4,6 +4,7 @@ import com.alibaba.dubbo.config.annotation.Service;
 import com.dubbo.api.service.CostService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service
 public class CostServiceImpl implements CostService {
@@ -12,6 +13,9 @@ public class CostServiceImpl implements CostService {
      * 假设之前总花费了100
      */
     private final Integer totalCost = 1000;
+
+    @Value("${server.port}")
+    private Integer port;
 
     /**
      * 之前总和 加上 最近一笔
@@ -26,11 +30,7 @@ public class CostServiceImpl implements CostService {
     })
     @Override
     public Integer add(int cost) {
-        try {
-            Thread.sleep(3000L);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return totalCost + cost;
+        /*return totalCost + cost;*/
+        return port;
     }
 }
